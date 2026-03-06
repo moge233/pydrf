1b1f82384b2902934015f4582cb00cfeb3f4749b
mathe <mathewlogan90@gmail.com>
2026-03-06 16:14:47 -0500

    Some data is multiplied by 100 in the text chart file, for some reason, so these changes divide some of those numbers by 100 to make them more normal.

--- src/pydrf/starter.py
+++ src/pydrf/starter.py
@@ -8,6 +8,21 @@ class Starter:
     def __init__(self, data: StarterPerformanceData):
         self.data: StarterPerformanceData = data
 
+        # Make the data more human readable/usable
+        self.data.odds /= 100
+        self.length_ahead_at_poc1 /= 100
+        self.length_ahead_at_poc2 /= 100
+        self.length_ahead_at_poc3 /= 100
+        self.length_ahead_at_poc4 /= 100
+        self.length_ahead_at_poc5 /= 100
+        self.length_ahead_at_finish /= 100
+        self.length_behind_at_poc1 /= 100
+        self.length_behind_at_poc2 /= 100
+        self.length_behind_at_poc3 /= 100
+        self.length_behind_at_poc4 /= 100
+        self.length_behind_at_poc5 /= 100
+        self.length_behind_at_finish /= 100
+
     def __str__(self):
         ret = ''
         for k, v in vars(self).items():
